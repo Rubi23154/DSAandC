d14d69535f7d25abf4802a9ae5d5b8c4de7f20c6
Rubi23514 <rubi26525@gmail.com>
2024-06-15 19:43:45 +0530

    add more file

---
+++ lecture37.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class solution{
+private:
+void solve(vector<int>nums ,vector<int>output,int index,vector<vector<int>>&ans){
+if(index>=nums.size()){
+    ans.push_back(output);
+    return;
+}
+//exclude
+solve(nums,output,index+1,ans);
+//for include..
+int element=nums[index];
+output.push_back(element);
+solve(nums,output,index+1,ans);
+}
+public:
+vector<vector<int>>subsets(vector<int>&nums){
+vector<vector<int>>ans;
+vector<int>output;
+int index=0;
+solve(nums,output,index,ans);
+return ans;
+}
+};
+//to find power of subsets using recursion
+int main(){
+    
+return 0;
+}
